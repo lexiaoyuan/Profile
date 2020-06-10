4541b982c95d623104dbc5c2b5a0db00695b3abc
lexiaoyuan <2775886918@qq.com>
2020-06-10 19:45:06 +0800

    fixed a bug

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -2,29 +2,27 @@
   <b-container fluid>
     <b-row><p class="font-weight-bold text-monospace">教育经历</p></b-row>
     <b-row class="border-top p-0">
-      <b-list-group horizontal class="flex-fill ml-2 text-monospace">
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+      <b-list-group horizontal class="ml-2 flex-fill text-monospace">
+        <b-list-group-item class="border-0 pb-0 w-25">
           {{ startTime }}-{{ endTime }}
         </b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+        <b-list-group-item class="border-0 pb-0 w-25">
           {{ school }}
         </b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+        <b-list-group-item class="border-0 pb-0 w-25">
           {{ major }}
         </b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+        <b-list-group-item class="border-0 pb-0 w-25">
           {{ grade }}
         </b-list-group-item>
       </b-list-group>
-      <b-list-group horizontal class="ml-2 flex-fill text-monospace">
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+      <b-list-group horizontal class="ml-2 text-monospace flex-fill">
+        <b-list-group-item class="border-0 pb-0 w-25">
           绩点：{{ gradePoint }}
         </b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-fill">
+        <b-list-group-item class="border-0 pb-0 w-25">
           成绩排名：{{ gradeDetail }}
         </b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-grow-1"></b-list-group-item>
-        <b-list-group-item class="border-0 pb-0 flex-grow-1"></b-list-group-item>
       </b-list-group>
     </b-row>
   </b-container>
